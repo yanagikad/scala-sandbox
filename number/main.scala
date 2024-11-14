ea16b79c7e8ba3e6971e9194526fe6eb9d10423b
yanagikad <thekykun+git@gmail.com>
2024-11-14 23:19:29 +0900

    feat: add var operations

---
+++ number/main.scala
@@ -0,0 +1,8 @@
+object var_operations {
+    def main(args: Array[String]) = {
+        val num: Int = 10
+        val num2: Int = 20
+        val num3: Float = 10.5f
+        println(s"Addition: ${num + num3}") // change Float
+    }
+}
